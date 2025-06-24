3de50e736637ff93f822b58bdc113252bf3a156f
ZHU Jiachen <jiachen.zhu@connect.polyu.hk>
2025-06-24 15:15:08 +0800

    files for visual studio

---
+++ vs-mingw.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME         Windows)
+set(CMAKE_C_COMPILER          "C:/msys64/ucrt64/bin/clang.exe")
+set(CMAKE_CXX_COMPILER        "C:/msys64/ucrt64/bin/clang++.exe")
+set(CMAKE_MAKE_PROGRAM        "C:/msys64/ucrt64/bin/ninja.exe")
+
+set(CMAKE_FIND_ROOT_PATH      "C:/msys64/ucrt64" "C:/msys64/usr")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
